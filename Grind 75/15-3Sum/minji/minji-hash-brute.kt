aad0f3c92f2620db7f685f2b433fdda1ffd8dfa2
Minji Kim <alswlzhsclq@naver.com>
2024-11-07 17:33:25 +0900

    minji

--- Grind 75/15-3Sum/minji/minji-hash-brute.kt
+++ Grind 75/15-3Sum/minji/minji-hash-brute.kt
@@ -1,16 +1,19 @@
 class Solution {
     fun threeSum(nums: IntArray): List<List<Int>> {
         val result = mutableSetOf<List<Int>>()
+        val nums = nums.sorted()
 
         for (i in 0..<(nums.size-2)) {
+            if (nums[i] > 0) break
+
             val target = nums[i]
-            val cache = hashMapOf<Int, Int>()
+            val cache = mutableSetOf<Int>()
 
             for (j in (i+1)..<nums.size) {
                 if (-target - nums[j] in cache) {
-                    result.add(listOf(target, -target - nums[j], nums[j]).sorted())
+                    result.add(listOf(target, -target - nums[j], nums[j]))
                 }
-                cache[nums[j]] = j
+                cache.add(nums[j])
             }
         }
 
